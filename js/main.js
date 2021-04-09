2c0005b2a2fe59731d8a86cfe3ecd6102734f4bc
Neko-programmer <55989479+Vladislav-2001@users.noreply.github.com>
2021-04-09 14:06:05 +0300

    Добавлены правила для адаптации

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {slidesPerView: 6},
+    1600: {slidesPerView: 5},
+    1300: {slidesPerView: 4},
+    1100: {slidesPerView: 3},
+    800: {slidesPerView: 2}
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
@@ -12,7 +20,12 @@ const swiper = new Swiper('.channel-slider', {
 const swiperRecommended = new Swiper('.recomended-video', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+    breakpoints: {
+    1700: {slidesPerView: 3},
+    1100: {slidesPerView: 2}
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.recomended-button-next',
@@ -20,13 +33,33 @@ const swiperRecommended = new Swiper('.recomended-video', {
   },
 });
 
-const swiperChRecommended = new Swiper('.recomended-channel', {
+const swiper2 = new Swiper('.recommended-channel', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {slidesPerView: 6},
+    1600: {slidesPerView: 5},
+    1300: {slidesPerView: 4},
+    1100: {slidesPerView: 3},
+    800: {slidesPerView: 2}
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.ch-recomended-button-next',
     prevEl: '.ch-recomended-button-prev',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640){
+  swiper.destroy();
+  swiperRecommended.destroy();  
+swiper2.destroy();
+};
